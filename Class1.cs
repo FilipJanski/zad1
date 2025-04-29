c20397df9a33b17d6dc4a39f16cb089e8ee85fd5
fifi <fifi@DESKTOP-CDGKJC6>
2025-04-29 21:16:09 +0200

    Dodaj pliki projektów.

---
+++ Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad1
+{
+    internal class Trapez
+    {
+        private double a;
+        private double b;
+        private double surface_area;
+        private double h;
+
+        public void read_data()
+        {
+            Console.Write("podaj podstawe a: ");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("podaj podstawe b: ");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("podaj wysokosc h: ");
+            h = Convert.ToDouble(Console.ReadLine());
+        }
+        private double process_data()
+        {
+            return surface_area=(a+b)*h/2;
+        }
+        public void show_results()
+        {
+            process_data();
+            System.Console.WriteLine(surface_area);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Trapez trapez = new Trapez();
+            trapez.read_data();
+            trapez.show_results();
+            Console.ReadKey();
+        }
+    }
+}
